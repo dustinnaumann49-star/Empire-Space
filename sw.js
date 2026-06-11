8761f054a7c7ee3b6a649f59a09e1c05a2f8fecc
dustinnaumann49-star <dustinnaumann49@gmail.com>
2026-06-11 23:22:21 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'ogame-exp-v1';
+const assets = ['index.html', '1000037890.jpg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
